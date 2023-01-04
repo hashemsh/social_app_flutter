db9232ba3791e91fec64b0f3d6c9cfef4df4e61e
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-01-04 22:19:24 +0330

    make login screen

---
+++ lib/screens/login_screen.dart
@@ -0,0 +1,132 @@
+import 'package:flutter/material.dart';
+
+class LoginScreen extends StatefulWidget {
+  const LoginScreen({Key key}) : super(key: key);
+
+  @override
+  State<LoginScreen> createState() => _LoginScreenState();
+}
+
+class _LoginScreenState extends State<LoginScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
+    final ThemeData themeData = Theme.of(context);
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: size.height,
+          child: Column(
+            children: [
+              Image(
+                height: size.height / 2.5,
+                width: double.infinity,
+                image: const AssetImage('assets/images/login_background.jpg'),
+                fit: BoxFit.cover,
+              ),
+              Text(
+                'FRENZY',
+                style: TextStyle(
+                    color: themeData.primaryColor,
+                    fontSize: 34.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 3.1),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Username',
+                    prefixIcon: Icon(
+                      Icons.account_box,
+                      size: 30.0,
+                    ),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 10.0,
+              ),
+              const Padding(
+                padding: EdgeInsets.symmetric(vertical: 10.0, horizontal: 20.0),
+                child: TextField(
+                  decoration: InputDecoration(
+                    contentPadding: EdgeInsets.symmetric(vertical: 15.0),
+                    fillColor: Colors.white,
+                    filled: true,
+                    hintText: 'Password',
+                    prefixIcon: Icon(
+                      Icons.lock,
+                      size: 30.0,
+                    ),
+                  ),
+                  obscureText: true,
+                ),
+              ),
+              const SizedBox(
+                height: 40.0,
+              ),
+              GestureDetector(
+                onTap: () {},
+                child: Container(
+                  margin: const EdgeInsets.symmetric(horizontal: 60.0),
+                  alignment: Alignment.center,
+                  height: 45.0,
+                  decoration: BoxDecoration(
+                    color: themeData.primaryColor,
+                    borderRadius: BorderRadius.circular(10.0),
+                  ),
+                  child: const Text(
+                    'Login',
+                    style: TextStyle(
+                        color: Colors.white,
+                        fontSize: 22.0,
+                        fontWeight: FontWeight.w600,
+                        letterSpacing: 1.5),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Align(
+                  alignment: FractionalOffset.bottomCenter,
+                  child: GestureDetector(
+                    onTap: () {},
+                    child: Container(
+                      alignment: Alignment.center,
+                      color: themeData.primaryColor,
+                      height: 80.0,
+                      child: RichText(
+                        text: const TextSpan(
+                          text: 'Don\'t have an account ? ',
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w300),
+                          children: [
+                            TextSpan(
+                              text: 'Sign up',
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 20.0,
+                                  fontWeight: FontWeight.w600),
+                            )
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
