c1226a05d943b04675cedc35e9f667e27d5909c4
hashem sheikhypour <sh.sheikhypour@gmail.com>
2023-01-04 23:46:17 +0330

    fix Navigator and status bar Color

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:social_app_flutter/screens/login_screen.dart';
 
 void main() {
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+    statusBarColor: Colors.transparent,
+    statusBarIconBrightness: Brightness.dark,
+    systemNavigationBarColor: Colors.blue,
+    systemNavigationBarIconBrightness: Brightness.light,
+  ));
   runApp(MyApp());
 }
 
